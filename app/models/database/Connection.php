28d426ab7b9aba21976663766816443e173e6c66
Админ <example@google.com>
2019-02-10 00:39:42 +0300

    try/catch in QueryBuilder

--- app/models/database/Connection.php
+++ app/models/database/Connection.php
@@ -3,14 +3,13 @@
 class Connection {
 	public static function make($config){
 		try{
-			$pdo = new PDO("{$config['connection']};dbname={$config['database']};charset={$config['charset']};", 
+			return new PDO("{$config['connection']};dbname={$config['database']};charset={$config['charset']};", 
 				$config['username'], 
 				$config['password']
 			);
+
 		}catch(PDOException $e){
 			die("Can not connect to the database");
 		}
-
-		return $pdo;
 	}
 }
--- app/models/database/QueryBuilder.php
+++ app/models/database/QueryBuilder.php
@@ -9,60 +9,85 @@ class QueryBuilder {
 	}
 
 	public function getAll($table){
-	
-		$sql = "SELECT * FROM {$table}";
-		$statement = $this->pdo->prepare($sql);
-		$statement->execute();
 
-		return $statement->fetchAll(PDO::FETCH_ASSOC);
+		try{
+			$sql = "SELECT * FROM {$table}";
+			$statement = $this->pdo->prepare($sql);
+			$statement->execute();
+
+			return $statement->fetchAll(PDO::FETCH_ASSOC);
+
+		}catch(PDOException $e){
+			die("Query error");
+		}
 	}
 
 	public function getOne($table, $id){
 
-		$sql = "SELECT * FROM {$table} WHERE id=:id";
-		$statement = $this->pdo->prepare($sql);
-		$statement->execute([
-			'id' => $id
-		]);
+		try{
+			$sql = "SELECT * FROM {$table} WHERE id=:id";
+			$statement = $this->pdo->prepare($sql);
+			$statement->execute([
+				'id' => $id
+			]);
 
-		return $statement->fetch(PDO::FETCH_ASSOC);
+			return $statement->fetch(PDO::FETCH_ASSOC);
+
+		}catch(PDOException $e){
+			die("Query error");
+		}
 	}
 
 	public function create($table, $data){
 
-		$keys = implode(',', array_keys($data));
-		$tags = ':' . implode(', :', array_keys($data));
+		try{
+			$keys = implode(',', array_keys($data));
+			$tags = ':' . implode(', :', array_keys($data));
 
-		$sql = "INSERT INTO {$table} ({$keys}) values ({$tags})";
-		$statement = $this->pdo->prepare($sql);
-		$statement->execute($data);
+			$sql = "INSERT INTO {$table} ({$keys}) values ({$tags})";
+			$statement = $this->pdo->prepare($sql);
+			$statement->execute($data);
+
+		}catch(PDOException $e){
+			die("Query error");
+		}
 	}
 
 	public function update($table, $data, $id){
 
-		$keys = array_keys($data);
+		try{
 
-		$string = '';
-		foreach ($keys as $key) {
-			$string .= $key . '=:' . $key . ',';
+			$keys = array_keys($data);
+
+			$string = '';
+			foreach ($keys as $key) {
+				$string .= $key . '=:' . $key . ',';
+			}
+
+			$keys = rtrim($string, ',');
+
+			$data['id'] = $id;
+
+			$sql = "UPDATE {$table} SET {$keys} WHERE id=:id";
+			$statement = $this->pdo->prepare($sql);
+			$statement->execute($data);
+
+		}catch(PDOException $e){
+			die("Query error");
 		}
-
-		$keys = rtrim($string, ',');
-
-		$data['id'] = $id;
-
-		$sql = "UPDATE {$table} SET {$keys} WHERE id=:id";
-		$statement = $this->pdo->prepare($sql);
-		$statement->execute($data);
 	}
 
 	public function delete($table, $id){
+		try{
+			$sql = "DELETE FROM {$table} WHERE id=:id";
+			$statement = $this->pdo->prepare($sql);
+			$statement->execute([
+				'id' => $id
+			]);
 
-		$sql = "DELETE FROM {$table} WHERE id=:id";
-		$statement = $this->pdo->prepare($sql);
-		$statement->execute([
-			'id' => $id
-		]);
+		}catch(PDOException $e){
+			die("Query error");
+		}
 	}
 }
 
